6722f5f192de92b9deecb4a2b195a4668f9541da
KinjiKawaguchi <kawakin0310@icloud.com>
2023-04-19 22:50:22 +0900

    基礎的な変数の扱い方を学習

---
+++ src/variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String args[])    {
+        System.out.print("Boushitu Monster");
+        /*int a = 1;
+        int b = 5;
+        System.out.println(a * b);
+        System.out.println(a + b);
+
+        String c = "BOU";
+        String d = "SHITU";
+        System.out.println(c + d);
+        System.out.println(d + c);
+
+        System.out.println(c + d + "Java勉強会");
+        */
+        String NPC_name = "博士";
+        System.out.println(NPC_name+"「お主の名前はなんじゃね？」");
+    }
+}
